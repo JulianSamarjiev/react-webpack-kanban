36ebb41081d42cd918930755bb108207cb99386e
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-07-31 22:14:38 +0200

    extracting notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,28 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note';
+import Notes from './Notes';
 
 export default class App extends React.Component {
   render() {
-    return <Note />;
+    const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn webpack'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do laundry'
+      }
+    ];
+
+    return (
+      <div>
+        <Notes items={notes} />
+      </div>
+    );
   }
 }
---
+++ app/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Note from './Note';
+
+export default class Notes extends React.Component {
+  render() {
+    const notes = this.props.items;
+
+    return <ul className="notes">{notes.map(this.renderNote)}</ul>;
+  }
+  renderNote(note) {
+    return (
+      <li className="note" key={`note${note.id}`}>
+        <Note value={note.task} />
+      </li>
+    );
+  }
+}
