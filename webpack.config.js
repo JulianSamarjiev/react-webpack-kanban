68365539624dca4c04e9d001be8a56c3af455da7
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-07-31 21:38:25 +0200

    add babel and react

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ var TARGET = process.env.TARGET;
 var ROOT_PATH = path.resolve(__dirname);
 
 var common = {
-  entry: [path.resolve(ROOT_PATH, 'app/main')],
+  entry: path.resolve(ROOT_PATH, 'app/main'),
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
@@ -26,14 +29,17 @@ var common = {
   ]
 };
 
-if(TARGET === 'build') {
-  module.exports = merge(common, {
-    devtool: 'source-map'
-  });
-}
-
 if(TARGET === 'dev') {
   module.exports = merge(common, {
-    devtool: 'eval'
+    devtool: 'eval',
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['babel?stage=1'],
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    }
   });
 }
