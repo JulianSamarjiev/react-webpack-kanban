117000ebfe7f332a92b81ada8d630f36bd8ddb47
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-07-31 21:50:09 +0200

    create first component, add react hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ if(TARGET === 'dev') {
       loaders: [
         {
           test: /\.jsx?$/,
-          loaders: ['babel?stage=1'],
+          loaders: ['react-hot', 'babel?stage=1'],
           include: path.resolve(ROOT_PATH, 'app')
         }
       ]
