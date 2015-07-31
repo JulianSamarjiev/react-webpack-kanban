d3536532e5b8126c5280e86dc7f9aa07ecadc8ca
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-07-31 21:26:56 +0200

    initial webpack configuration

---
+++ app/main.js
@@ -0,0 +1,8 @@
+require('./stylesheets/main.css');
+
+var component = require('./component');
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
