987ceb54cbec606c77047c48ca6c70e3c4c42aa2
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-08-02 18:40:26 +0200

    Editing Notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,28 +1,60 @@
 import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes';
+import findIndex from '../libs/find_index';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    }
+
+    this.addItem = this.addItem.bind(this);
+    this.itemEdited = this.itemEdited.bind(this);
+  }
   render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+    const notes = this.state.notes;
 
     return (
       <div>
-        <Notes items={notes} />
+        <button onClick={this.addItem}>+</button>
+        <Notes items={notes} onEdit={this.itemEdited} />
       </div>
     );
+  }
+  addItem() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+  itemEdited(noteId, task) {
+    let notes = this.state.notes;
+    const noteIndex = findIndex(notes, 'id', noteId);
+
+    if (noteIndex < 0) {
+      return console.warn('Failed to find note', notes, noteId);
+    }
+
+    notes[noteIndex].task = task;
+
+    this.setState({notes});
   }
 }
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Note from './Note';
 
 export default class Notes extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderNote = this.renderNote.bind(this);
+  }
   render() {
     const notes = this.props.items;
 
@@ -10,7 +15,9 @@ export default class Notes extends React.Component {
   renderNote(note) {
     return (
       <li className="note" key={`note${note.id}`}>
-        <Note value={note.task} />
+        <Note
+          value={note.task}
+          onEdit={this.props.onEdit.bind(null, note.id)} />
       </li>
     );
   }
